bd8894f52f91cc4fc987cd1e6e2b321a65471fb4
Peyman <pjahanbazi@gmail.com>
2022-04-26 17:42:49 +0430

    menu bar is created

--- src/public-site/src/components/MenuBar.jsx
+++ src/public-site/src/components/MenuBar.jsx
@@ -1,32 +1,40 @@
+import "./MenuBar.scss";
+import logo from "./logo.svg";
+
 function MenuBar() {
+  const menyItems = [
+    { text: "Home", href: "" },
+    { text: "What is ID", href: "" },
+    { text: "About us", href: "" },
+    { text: "Contact us", href: "" },
+    { text: "FAQ", href: "" },
+  ];
+  const menuItemsHtml = menyItems.map((x) => (
+    <li className="nav-item">
+      <a className="nav-link active" href={x.href}>
+        {x.text}
+      </a>
+    </li>
+  ));
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav id="menubar" className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Brand
+          <img src={logo} className="imglogo" />
         </a>
         <div className="collapse navbar-collapse" id="navbarContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
+          <ul className=" menuitems navbar-nav me-auto mb-2 mb-lg-0">
+            {menuItemsHtml}
           </ul>
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Register
+              <a className=" loginLink nav-link" href="#">
+                Login
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Login
+              <a className=" registerLink nav-link" href="#">
+                Register
               </a>
             </li>
           </ul>
